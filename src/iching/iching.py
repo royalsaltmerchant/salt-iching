113d08198caf951ea97de60890ab43f6c1975979
Julian ranieri <jumpingafterrain@gmail.com>
2021-05-08 16:11:38 -0700

    dictionary working

--- src/iching/iching.py
+++ src/iching/iching.py
@@ -5,12 +5,15 @@ with open('../../assets/data.json') as d:
   data = json.load(d)
 
 def get_hexagram_by_number(number:int):
+  hexagram_by_number_dict = {}
+
   for hexagram in data['hexagrams']:
     if number == hexagram['number']:
       for i in hexagram:
         # print(i, hexagram[i])
-        hexagram_by_number_dict = {i: hexagram[i]}
-        print(hexagram_by_number_dict)
+        hexagram_by_number_dict[i] = hexagram[i] 
+  
+  print(hexagram_by_number_dict)
 
 get_hexagram_by_number(7)
 
