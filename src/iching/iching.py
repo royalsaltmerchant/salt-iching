826bc12bebaeeee471a61d74a05dea73103d9caa
Julian ranieri <jumpingafterrain@gmail.com>
2021-05-09 08:19:14 -0700

    hexagram random

--- src/iching/iching.py
+++ src/iching/iching.py
@@ -1,4 +1,5 @@
 import json
+from random import randrange
 
 #get iching data
 with open('../../assets/data.json') as d:
@@ -13,8 +14,36 @@ def get_hexagram_by_number(number:int):
         # print(i, hexagram[i])
         hexagram_by_number_dict[i] = hexagram[i] 
   
-  print(hexagram_by_number_dict)
+  # print(hexagram_by_number_dict)
+  return hexagram_by_number_dict
 
-get_hexagram_by_number(7)
+# get_hexagram_by_number(7)
+
+def get_hexagram_by_lines(lines):
+  hexagram_by_lines_dict = {}
+
+  for hexagram in data['hexagrams']:
+    if lines == hexagram['lines']:
+      for i in hexagram:
+        # print(i, hexagram[i])
+        hexagram_by_lines_dict[i] = hexagram[i] 
+  
+  print(hexagram_by_lines_dict)
+
+# lines = [0, 1, 0, 0, 0 ,0]
+# get_hexagram_by_lines(lines)
+  return hexagram_by_lines_dict
+
+
+# def get_lines():
+
+def get_random_hexagram():
+  number = randrange(1, 64)
+  hexagram = get_hexagram_by_number(number)
+  # print(hexagram)
+  return hexagram
+
+get_random_hexagram()
+
 
 
